f6878d9a84eaaacd21fc11e700585fc7b39de0d8
HelvioSiqueira <helvio.vilhena-ext@ab-inbev.com>
2024-12-18 11:32:40 -0300

    remove imports

--- src/main/kotlin/com/workshop/Routing.kt
+++ src/main/kotlin/com/workshop/Routing.kt
@@ -1,18 +1,14 @@
 package com.workshop
 
-import com.workshop.model.TaskRepository
-import io.ktor.http.*
-import io.ktor.serialization.kotlinx.json.*
-import io.ktor.server.application.*
-import io.ktor.server.http.content.*
-import io.ktor.server.plugins.contentnegotiation.*
-import io.ktor.server.plugins.statuspages.*
-import io.ktor.server.request.*
-import io.ktor.server.response.*
-import io.ktor.server.routing.*
-import java.sql.Connection
-import java.sql.DriverManager
-import org.jetbrains.exposed.sql.*
+import io.ktor.http.HttpStatusCode
+import io.ktor.server.application.Application
+import io.ktor.server.application.install
+import io.ktor.server.http.content.staticResources
+import io.ktor.server.plugins.statuspages.StatusPages
+import io.ktor.server.response.respondText
+import io.ktor.server.routing.get
+import io.ktor.server.routing.post
+import io.ktor.server.routing.routing
 
 fun Application.configureRouting() {
     install(StatusPages) {
